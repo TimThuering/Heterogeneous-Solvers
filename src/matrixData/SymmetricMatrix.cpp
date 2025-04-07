6fed8df1ace3f7dfda40fb21689e21a6934f8307
TimThuering <56958925+TimThuering@users.noreply.github.com>
2025-04-07 18:40:41 +0200

    update symmetric matrix class

--- src/matrixData/SymmetricMatrix.cpp
+++ src/matrixData/SymmetricMatrix.cpp
@@ -4,6 +4,10 @@
 #include "SymmetricMatrix.hpp"
 #include "Configuration.hpp"
 
+SymmetricMatrix::SymmetricMatrix(const std::size_t N, const std::size_t blockSize): N(N), blockSize(blockSize)
+{
+}
+
 int SymmetricMatrix::example() {
   conf::fp_type value = 1.0f;
   std::cout << typeid(value).name() << std::endl;
--- src/matrixData/SymmetricMatrix.hpp
+++ src/matrixData/SymmetricMatrix.hpp
@@ -1,13 +1,20 @@
 #ifndef SYMMETRICMATRIX_HPP
 #define SYMMETRICMATRIX_HPP
 
+#include <vector>
+#include "Configuration.hpp"
 
-class SymmetricMatrix {
-  public:
+class SymmetricMatrix
+{
+public:
+    SymmetricMatrix(std::size_t N, std::size_t blockSize);
+
+    const std::size_t N; /// Size N of the NxN symmetric matrix
+    const std::size_t blockSize; /// The matrix will be partitioned in blockSize x blockSize blocks
+
+    std::vector<conf::fp_type> matrixData; /// internal matrix data structure
+
     int example();
-
 };
-
-
 
 #endif //SYMMETRICMATRIX_HPP
