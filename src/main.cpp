d64699e2d4fe903db2fca1ed904156b807dbdc8c
TimThuering <56958925+TimThuering@users.noreply.github.com>
2025-04-11 17:01:28 +0200

    integrate cxxopts

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sycl/sycl.hpp>
 #include <hws/system_hardware_sampler.hpp>
+#include <cxxopts.hpp>
 
 #include "MatrixParser.hpp"
 #include "SymmetricMatrix.hpp"
@@ -8,7 +9,7 @@
 
 using namespace sycl;
 
-int main()
+int main(int argc, char *argv[])
 {
 #ifdef USE_DOUBLE
     std::cout << "Using FP64 double precision" << std::endl;
@@ -16,12 +17,42 @@ int main()
     std::cout << "Using FP32 single precision" << std::endl;
 #endif
 
-    std::string path_A = "../matrixGenerator/A_20.txt";
-    std::string path_b = "../matrixGenerator/b_20.txt";
-    // std::string path2 = "../matrixGenerator/matrixBlocked.txt";
+
+    cxxopts::Options argumentOptions("Heterogeneous Conjugate Gradients", "CG Algorithm with CPU-GPU co-execution");
+
+    argumentOptions.add_options()
+        ("A,path_A", "path to .txt file containing symmetric positive definite matrix A", cxxopts::value<std::string>())
+        ("b,path_b", "path to .txt file containing the right-hand side b", cxxopts::value<std::string>())
+        ("o,output", "path to the output directory", cxxopts::value<std::string>())
+        ("d,device", "Specifies which devices the solver will use. Has to be 'cpu', 'gpu' or 'mixed'.",cxxopts::value<std::string>())
+    ;
+
+    const auto arguments = argumentOptions.parse(argc, argv);
+
+    std::string path_A;
+    std::string path_b;
+    if (arguments.count("path_A"))
+    {
+        path_A = arguments["path_A"].as<std::string>();
+    } else
+    {
+        throw std::runtime_error("No path to .txt file for matrix A specified");
+    }
+
+    if (arguments.count("path_b"))
+    {
+        path_b = arguments["path_b"].as<std::string>();
+    } else
+    {
+        throw std::runtime_error("No path to .txt file for right-hand side b specified");
+    }
+
 
     queue gpuQueue(gpu_selector_v);
     queue cpuQueue(cpu_selector_v);
+
+    std::cout << "GPU: " << gpuQueue.get_device().get_info<info::device::name>() <<  std::endl;
+    std::cout << "CPU: " << cpuQueue.get_device().get_info<info::device::name>() <<  std::endl;
 
     CG algorithm(path_A, path_b, cpuQueue, gpuQueue);
     algorithm.solve();
