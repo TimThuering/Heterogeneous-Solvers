35cc8a0f4290cdd7bd15d7d96ac42d44ce171229
TimThuering <56958925+TimThuering@users.noreply.github.com>
2025-04-11 16:06:33 +0200

    rename directory

--- src/kernel/MatrixVectorOperations.hpp
+++ src/kernel/MatrixVectorOperations.hpp
@@ -11,7 +11,7 @@ class MatrixVectorOperations
 public:
     static void matrixVectorBlock(sycl::queue& queue, const conf::fp_type* A, const conf::fp_type* b, conf::fp_type* result,
                                   int blockStart_i, int blockStart_j, int blockCount_i, int blockCount_j, int blockCountXY);
-    
+
 };
 
 
