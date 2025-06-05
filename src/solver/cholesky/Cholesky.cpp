09a11d67b872d17b4202d17b2c2387891a465900
TimThuering <56958925+TimThuering@users.noreply.github.com>
2025-06-05 09:33:08 +0200

    added time tracking to cholesky

--- src/solver/cholesky/Cholesky.cpp
+++ src/solver/cholesky/Cholesky.cpp
@@ -11,6 +11,7 @@ Cholesky::Cholesky(SymmetricMatrix& A, queue& cpuQueue, queue& gpuQueue):
 }
 
 void Cholesky::solve() {
+    const auto start = std::chrono::steady_clock::now();
     // init GPU data structure for matrix A on which the Cholesky decomposition should be performed
     A_gpu = malloc_device<conf::fp_type>(A.matrixData.size(), gpuQueue);
 
@@ -24,37 +25,76 @@ void Cholesky::solve() {
 
     // begin with tiled Cholesky decomposition using right-looking algorithm
     for (int k = 0; k < A.blockCountXY; ++k) {
-        std::cout << "Column: " << k << std::endl;
+        auto startColumn = std::chrono::steady_clock::now();
+
+        auto startCholesky = std::chrono::steady_clock::now();
+        auto endCholesky = startCholesky;
+
+        auto startTriangularSolve = std::chrono::steady_clock::now();
+        auto endTriangularSolve = startTriangularSolve;
+
+        auto startMatrixMatrixDiagonal = std::chrono::steady_clock::now();
+        auto endMatrixMatrixDiagonal = startMatrixMatrixDiagonal;
+
+        auto startMatrixMatrix = std::chrono::steady_clock::now();
+        auto endMatrixMatrix = startMatrixMatrix;
+
         // ID of diagonal block A_kk
         const int columnsToRight = A.blockCountXY - k;
         const int blockID = blockCountATotal - (columnsToRight * (columnsToRight + 1) / 2);
 
         // perform Cholesky decomposition on diagonal block A_kk
-        MatrixOperations::cholesky(gpuQueue, A_gpu, blockID, k);
+        startCholesky = std::chrono::steady_clock::now();
+        MatrixOperations::cholesky_GPU_optimized(gpuQueue, A_gpu, blockID, k);
         gpuQueue.wait();
+        endCholesky = std::chrono::steady_clock::now();
 
         if (k < A.blockCountXY - 1) {
             // solve triangular system for current column k below the diagonal
-            MatrixMatrixOperations::triangularSolve(gpuQueue, A_gpu, blockID, k, k + 1,A.blockCountXY - (k + 1));
+            startTriangularSolve = std::chrono::steady_clock::now();
+            MatrixMatrixOperations::triangularSolve_optimizedGPU(gpuQueue, A_gpu, blockID, k, k + 1,A.blockCountXY - (k + 1));
             gpuQueue.wait();
+            endTriangularSolve = std::chrono::steady_clock::now();
 
             // update the blocks on the diagonal below the current diagonal block
+            startMatrixMatrixDiagonal = std::chrono::steady_clock::now();
             MatrixMatrixOperations::symmetricMatrixMatrixDiagonal_optimizedGPU(gpuQueue, A_gpu, blockID, k, k + 1,A.blockCountXY - (k + 1), A.blockCountXY);
             gpuQueue.wait();
+            endMatrixMatrixDiagonal = std::chrono::steady_clock::now();
 
             if (k < A.blockCountXY - 2) {
                 // update the blocks int the lower triangle below the current diagonal block
+                startMatrixMatrix = std::chrono::steady_clock::now();
                 MatrixMatrixOperations::matrixMatrixStep_optimizedGPU(gpuQueue, A_gpu, blockID, k, k + 2,A.blockCountXY - (k + 2), A.blockCountXY);
                 gpuQueue.wait();
+                endMatrixMatrix = std::chrono::steady_clock::now();
             }
         }
+
+        auto endColumn = std::chrono::steady_clock::now();
+        auto columnTime = std::chrono::duration<double, std::milli>(endColumn - startColumn).count();
+
+        auto choleskyTime = std::chrono::duration<double, std::milli>(endCholesky - startCholesky).count();
+        auto triangularSolveTime = std::chrono::duration<double, std::milli>(endTriangularSolve - startTriangularSolve).count();
+        auto matrixMatrixDiagonalTime = std::chrono::duration<double, std::milli>(endMatrixMatrixDiagonal - startMatrixMatrixDiagonal).count();
+        auto matrixMatrixTime = std::chrono::duration<double, std::milli>(endMatrixMatrix - startMatrixMatrix).count();
+
+        std::cout << "Column: " << k << ": " << columnTime << "ms" << std::endl;
+        std::cout << "   -- cholesky:               " << choleskyTime << "ms" << std::endl;
+        std::cout << "   -- triangular solve:       " << triangularSolveTime << "ms" <<  std::endl;
+        std::cout << "   -- matrix-matrix diagonal: " << matrixMatrixDiagonalTime << "ms" << std::endl;
+        std::cout << "   -- matrix-matrix:          " << matrixMatrixTime << "ms" << std::endl;
+        std::cout << std::endl;
+        std::cout << std::endl;
     }
 
     gpuQueue.submit([&](handler& h) {
         h.memcpy(A.matrixData.data(), A_gpu, A.matrixData.size() * sizeof(conf::fp_type));
     }).wait();
 
+    const auto end = std::chrono::steady_clock::now();
+    const auto totalTime = std::chrono::duration<double, std::milli>(end - start).count();
+    std::cout << "Total time: " << totalTime << "ms" << std::endl;
+
     // MatrixParser::writeFullMatrix("./A_chol_result", A);
-
-
 }
